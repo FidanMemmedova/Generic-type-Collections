78b6eee6b62bda6b307b8b32fccf737180e07f6a
Fidan Mammadova <100687487+FidanMemmedova@users.noreply.github.com>
2022-04-10 20:03:20 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Generic_type__Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> Languages = new SortedList<int, string>();
+            Languages.Add(1990, "Python");
+            Languages.Add(1002, "C#");
+            Languages.Add(1995, "Java");
+            foreach (var item in Languages)
+            {
+                Console.WriteLine($"Created year: {item.Key}, Language:{item.Value}");
+            }
+            foreach (var item in Languages)
+            {
+                Languages.((x, y) => string.Compare(x.Value, y.Value));??????????
+            }
+
+
+
+        }
+        
+
+    }
+}
